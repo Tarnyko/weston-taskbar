f3a4b0ba0eabab6d9af712266b87cdbf23caf353
Kristian Høgsberg <krh@bitplanet.net>
2013-10-30 09:07:04 -0700

    animation.c: Schedule a repaint when the animation is done
    Otherwise we don't repaint with the final state of the surface and
    we're stuck with the second-to-last frame of the animation until
    something else (moving the mouse or such) triggers a redraw.
    
    https://bugs.freedesktop.org/show_bug.cgi?id=70930

--- src/animation.c
+++ src/animation.c
@@ -167,6 +167,7 @@ weston_surface_animation_frame(struct weston_animation *base,
 	weston_spring_update(&animation->spring, msecs);
 
 	if (weston_spring_done(&animation->spring)) {
+		weston_compositor_schedule_repaint(animation->surface->compositor);
 		weston_surface_animation_destroy(animation);
 		return;
 	}
